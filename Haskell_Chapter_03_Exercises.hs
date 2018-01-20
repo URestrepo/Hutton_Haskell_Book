14053d2727579ccf6b2da95893ccc4ebd8a21d81
Uriel Restrepo <31256964+URestrepo@users.noreply.github.com>
2018-01-20 12:24:37 -0500

    Add Chapter 3

--- Haskell_Chapter_03_Exercises.hs
+++ Haskell_Chapter_03_Exercises.hs
@@ -2,39 +2,31 @@
 3.11 Exercises
 1. What are the types of the following values?
 
+
 ['a','b','c'] 
-list of Chars
 
-GHCi Answer
-['a','b','c'] :: [Char]
+['a','b','c'] :: [Char] -- list of Chars
 
-('a','b','c') 
-Tuple of arity 3
+('a','b','c')
 
-GHCi Answer
-('a','b','c') :: (Char, Char, Char)
+('a','b','c') :: (Char, Char, Char) -- Tuple of arity 3 
 
 [(False,'O'),(True,'1')]
-List of Tuple (Bool, Char) Arity 2
 
-GHCi Answer
-[(False,'O'),(True,'1')] :: [(Bool, Char)]
+[(False,'O'),(True,'1')] :: [(Bool, Char)] -- List of Tuple arity 2
 
 ([False,True],['0','1']) 
-Tuple of arity 2 ([Bool], [Char])
 
-GHCi Answer
-([False,True],['0','1']) :: ([Bool], [Char])
+([False,True],['0','1']) :: ([Bool], [Char]) -- Tuple of arity 2
 
 [tail, init, reverse]
-list of functions?
 
-GHCi Answer
-[tail, init, reverse] :: [[a] -> [a]]
+[tail, init, reverse] :: [[a] -> [a]] -- list of functions
+
+-}
 
 
-
-2. Write down definitions that have the following types; it does not matter what the definitions actually do as long as they are type correct.
+{- 2. Write down definitions that have the following types; it does not matter what the definitions actually do as long as they are type correct.-} 
 
 bools :: [Bool] 
 bools = [True]
@@ -51,35 +43,10 @@ copy a = (a,a)
 apply :: (a -> b) -> a -> b
 apply f x = f x
 
-3. What are the types of the following functions? Hint, create function definitions in form ::
+{- 3. What are the types of the following functions? Hint, create function definitions in form ::
+Hint: take care to include the necessary class constraints in the types if the functions are defined using overloaded operators. -}
 
-second xs = head (tail xs)
-:: [a] -> a
-
-
-swap (x,y) = (y,x) 
-:: (t, t1) -> (t1, t)
-
-
-pair x y = (x,y) 
-pair :: a -> b -> (a, b)
-
-double x = x*2
-double :: num a => a -> a
-
-palindrome xs = reverse xs == xs 
-:: Eq => [a] -> Bool
-
-twice' f x = f (f x)
-twice' :: ( a -> a) -> a
-
-Hint: take care to include the necessary class constraints in the types if the functions are defined using overloaded operators.
-
-4. Check your answers to the preceding three questions using GHCi.
-
-5. Why is it not feasible in general for function types to be instances of the Eq class? When is it feasible? Hint: two functions of the same type are equal if they always return equal results for equal arguments.
--}
-
+-- The question is actually the below the type definition. Question was reversed so that module can be loaded in Haskell
 
 add' :: Int -> Int -> Int -> Int
 add' a b c = a + b + c
@@ -93,17 +60,32 @@ twice' f x = f (f x)
 pair' :: a -> b -> (a, b)
 pair' x y = (x,y) 
 
-
 double' :: Num a => a -> a
 double' x = x*2
-
 
 palindrome' :: Eq a => [a] -> Bool
 palindrome' xs = reverse xs == xs 
 
-
 second' :: [a] -> a
 second' xs = head (tail xs)
+
+
+{-
+
+4. Check your answers to the preceding three questions using GHCi.
+
+5. Why is it not feasible in general for function types to be instances of the Eq class? 
+When is it feasible? 
+Hint: two functions of the same type are equal if they always return equal results for equal arguments.
+
+To ask if two functions are the same is to ask the computer for a math proof that the functions are equivalent. 
+If it cannot prove, it would also be difficult for computer to check every possible variation of inputs 
+and check if they have the same output.
+
+
+-}
+
+
 
 
 

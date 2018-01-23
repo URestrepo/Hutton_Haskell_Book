599ad9ced8332009f4084c2439746dbd354acdd8
Uriel Restrepo <31256964+URestrepo@users.noreply.github.com>
2018-01-22 23:13:46 -0500

    Add Chapter 7

---
+++ Haskell_Chapter_07_Notes.hs
@@ -0,0 +1,32 @@
+
+
+-- Haskell
+--Chapter 7
+-- Higher-order functions
+
+{-
+------------------ Basic Concepts -----------------
+Higher-order functions allow common programming patterns to be encapsulated as functions
+
+-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
